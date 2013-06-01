c8b1184793a13280f6df6254bb5622a361139abd
gjz010 <gjz010944@gmail.com>
2013-06-01 17:40:35 +0900

    Create LoginFrame.java

---
+++ src/main/java/com/nekocraft/launcher/LoginFrame.java
@@ -0,0 +1,10 @@
+package com.nekocraft.launcher
+import java.awt.*;  
+import java.awt.event.*;  
+import javax.swing.*;  
+public class LoginFrame extends JFrame{
+    public LoginFrame(){
+        this.setSize(new Dimension(400, 500));  
+        this.setTitle("Nekocraft Launcher");  
+    }
+}
