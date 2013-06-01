408253a46d35739369676c06f2d6efed9d033b7b
gjz010 <gjz010944@gmail.com>
2013-06-01 17:42:09 +0900

    Update Launcher.java

--- src/main/java/com/nekocraft/launcher/Launcher.java
+++ src/main/java/com/nekocraft/launcher/Launcher.java
@@ -1,6 +1,8 @@
 package com.nekocraft.launcher;
 public class Launcher{
-  public static void main(String[] args){
-    System.out.println("Woo Nekocraft Launcher!");
-  }
+    public static void main(String[] args){
+        System.out.println("Woo Nekocraft Launcher!");
+        LoginFrame mf=new LoginFrame();
+        mf.setVisible(true)
+    }
 }
