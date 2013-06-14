6cdbbb5e9ede9cfea6cead41bd8d3b7a3d12827c
gjz010 <gjz010944@gmail.com>
2013-06-14 12:20:58 +0800

    ff

--- src/main/java/com/nekocraft/launcher/LoginThread.java
+++ src/main/java/com/nekocraft/launcher/LoginThread.java
@@ -3,7 +3,8 @@
  * and open the template in the editor.
  */
 package com.nekocraft.launcher;
-
+import java.io.*;
+import java.net.*;
 /**
  *
  * @author Administrator
@@ -11,6 +12,35 @@ package com.nekocraft.launcher;
 public class LoginThread implements Runnable{
     public static final String API="https://nekocraft.com/api/login/";
     //API usage : http://nekocraft.com/mclogin/?user=---&password=---
-    public void run(){
+    private String user;
+    private String password;
+    private URL u;
+    private HttpURLConnection con;
+    private String session;
+    public void run() {
+        try{
+            u=new URL(API);
+            con = (HttpURLConnection) u.openConnection();
+            con.setRequestMethod("POST");
+            con.setDoOutput(true);
+            con.setDoInput(true);
+            con.setUseCaches(false);
+            con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            OutputStreamWriter osw = new OutputStreamWriter(con.getOutputStream(), "UTF-8");
+            osw.write("user="+user+"&password="+password);
+            osw.flush();
+            osw.close();
+            InputStream in=con.getInputStream();
+            int count = 0;
+            while (count == 0) {
+             count = in.available();
+             }
+             byte[] b = new byte[count];
+             in.read(b);
+            session=new String(b);
+        }
+        catch (Exception e){
+            NekoLauncher.handleException(e);
+        }
     }
 }
